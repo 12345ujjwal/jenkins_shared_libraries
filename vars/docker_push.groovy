53dda5a5f9a74a0fa7b770b897868c28f4d50b07
Ujjwal Pratap Singh <a-9609@kmclu.ac.in>
2026-09-11 19:28:21 +0530

    Create docker_push.groovy

---
+++ vars/docker_push.groovy
@@ -0,0 +1,9 @@
+def call(String Project, String ImageTag, String dockerHubUser){
+  withCredentials([usernamePassword(
+  credentialsId:'jenkin-dockerhub',
+  passwordVariable: 'dockerHubPass',
+  usernameVariable:'dockerHubUser')]){
+    sh 'docker login -u $dockerHubUser -p $dockerHubPass'
+    sh 'docker push $dockerHubUser/$Project:$ImageTag'
+  }
+}
