63bbc0f75e64b33fece1ef3fd25709a9dd666057
Ujjwal Pratap Singh <a-9609@kmclu.ac.in>
2026-09-11 19:51:01 +0530

    Fix string interpolation in docker push command

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -4,6 +4,6 @@ def call(String Project, String ImageTag){
   passwordVariable: 'dockerHubPass',
   usernameVariable:'dockerHubUser')]){
     sh 'docker login -u $dockerHubUser -p $dockerHubPass'
-    sh 'docker push $dockerHubUser/${Project}:${ImageTag}'
+    sh "docker push \$dockerHubUser/${Project}:${ImageTag}"
   }
 }
